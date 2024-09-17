05ccd568206899ac24319a945aac5ac51a33a443
tsunglung <tsunglung@github.com>
2024-09-17 17:02:56 +0800

    Fix deprecated async_forward_entry_setup

--- custom_components/xiaomi_airfryer/__init__.py
+++ custom_components/xiaomi_airfryer/__init__.py
@@ -108,8 +108,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data[DOMAIN][host] = fryer
 
     # init setup for each supported domains
-    for platform in DOMAINS:
-        hass.async_create_task(hass.config_entries.async_forward_entry_setup(
-            entry, platform))
+    await hass.config_entries.async_forward_entry_setups(entry, DOMAINS)
 
     return True
